349d34d2c9be55151828e2338eb30716fd8623b9
Johanot <67147458+SirJohanot@users.noreply.github.com>
2022-09-03 17:57:10 +0300

    initial commit

---
+++ src/test/java/com/patiun/thrillingtreks/application/ThrillingTreksApplicationTests.java
@@ -0,0 +1,13 @@
+package com.patiun.thrillingtreks.application;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ThrillingTreksApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
