d301634b1d349c614d242578c9046b9c2f72190f
Johanot <67147458+SirJohanot@users.noreply.github.com>
2022-09-08 21:29:50 +0300

    added user password encoding

--- src/main/java/com/patiun/thrillingtreks/user/UserService.java
+++ src/main/java/com/patiun/thrillingtreks/user/UserService.java
@@ -4,20 +4,24 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 
 @Service
 public class UserService implements UserDetailsService {
 
     private final UserRepository userRepository;
+    private final PasswordEncoder passwordEncoder;
 
     @Autowired
-    public UserService(UserRepository userRepository) {
+    public UserService(UserRepository userRepository, PasswordEncoder passwordEncoder) {
         this.userRepository = userRepository;
+        this.passwordEncoder = passwordEncoder;
     }
 
     public User signIn(String name, String password) {
-        return userRepository.save(new User(name, password));
+        String encodedPassword = passwordEncoder.encode(password);
+        return userRepository.save(new User(name, encodedPassword));
     }
 
     @Override
